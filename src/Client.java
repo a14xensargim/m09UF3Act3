a62ecde5b440dd704116242c3d860b0a016dff43
ausias <a14xensargim>
2022-03-24 10:36:38 +0100

    COMMIT

--- src/Client.java
+++ src/Client.java
@@ -1,60 +1,29 @@
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args){
-        iniciPrograma();
-    }
-
-    public static void iniciPrograma() {
-        boolean bool = true;
-        InputStream lector = null;
-        ProcessBuilder proces = new ProcessBuilder();
-        proces.command("java", "-jar", "JAR/m09UF3Act3.jar");
-
+    public static void main(String[] argv) throws Exception {
+        System.setProperty("javax.net.ssl.trustStore", "ClientTrustStore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "clientts");
         try {
-            Process b = proces.start();
-            PrintWriter escritura = new PrintWriter(b.getOutputStream());
-            lector = b.getInputStream();
-            BufferedReader entrada = new BufferedReader(new InputStreamReader(lector));
-
-            do {
-                // Hora, minut i segon del sistema.
-                long tempsMiliSegons = System.currentTimeMillis();
-                SimpleDateFormat formatData = new SimpleDateFormat("HH:mm:ss");
-                Date resultdate = new Date(tempsMiliSegons);
-
-                //
-                escritura.println(formatData.format(resultdate) + " - Hola");
-                escritura.flush();
-
-                sortidaConsola(entrada);
-
-                Thread.sleep(1000);
-
-            }while (bool);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                lector.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            SSLSocketFactory sslFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            SSLSocket cliSocket = (SSLSocket) sslFactory.createSocket("localhost", 4043);
+            Scanner reader = new Scanner(System.in);
+            PrintStream writer = new PrintStream(cliSocket.getOutputStream());
+            System.out.println("Deixa una línia en blanc per acabar:");
+            String text = reader.nextLine();
+            while (!text.equals("")) {
+                writer.println(text);
+                writer.flush();
+                text = reader.nextLine();
             }
+            writer.println("<<FI>>");
+            writer.flush();
+            cliSocket.close();
+        } catch (Exception ex) {
+            System.err.println("Error en les comuncacions: " + ex);
         }
-    }
-
-    /**
-     * Mètode que llegeix la línia
-     * @param entrada
-     * @throws IOException
-     */
-    public static void sortidaConsola(BufferedReader entrada) throws IOException {
-        String linia = "";
-        linia = entrada.readLine();
-
-        System.out.println(linia);
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,32 @@
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
 import java.util.Scanner;
 
 public class Server {
-    public static void main(String[] args){
-        boolean bool = true;
-        Scanner agafa = new Scanner(System.in);
-
-        do {
-            System.out.println("Ha dit: " + agafa.nextLine());
-
-
-        }while (bool);
+    public static void main(String[] argv) throws Exception {
+        System.setProperty("javax.net.ssl.keyStore", "ServerKeyStore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "serverks");
+        try {
+            SSLServerSocketFactory sslFactory = (SSLServerSocketFactory) SSLServerSocketFactory
+                    .getDefault();
+            SSLServerSocket srvSocket = (SSLServerSocket) sslFactory.createServerSocket(4043);
+            int numClient = 1;
+            while (true) {
+                SSLSocket cliSocket = (SSLSocket) srvSocket.accept();
+                Scanner reader = new Scanner(cliSocket.getInputStream());
+                String text = reader.nextLine();
+                while (!text.equals("<<FI>>")) {
+                    System.out.println("[Client " + numClient + "] " + text);
+                    System.out.flush();
+                    text = reader.nextLine();
+                }
+                System.out.println("[Client " + numClient+ "] Tancant connexió...");
+                cliSocket.close();
+                numClient++;
+            }
+        } catch (Exception ex) {
+            System.err.println("Error en les comuncacions: " + ex);
+        }
     }
 }
